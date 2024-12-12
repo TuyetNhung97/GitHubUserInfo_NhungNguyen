0dd163ca1ee750d51a252fc496fc93055c59a5a8
NhungNTT47 <nhungntt47@fpt.com>
2024-12-13 00:01:40 +0700

    Initial Commit

---
+++ GitHubUserInfo_NhungNguyen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUserInfo_NhungNguyen
+//
+//  Created by Nguyen Jenny on 13/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
